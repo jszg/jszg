bd8406e1cbce0749e57109a157939ae81074daff
xtuer <biao.mac@icloud.com>
2016-10-18 15:44:00 +0800

    urls 定义在 urls.js，方便管理

---
+++ src/main/webapp/WEB-INF/static/js/urls.js
@@ -0,0 +1,4 @@
+Urls = {
+    REST_SUBJECTS_ROOT: '/rest/signUp/provinces/{provinceId}/certTypes/{certTypeId}/subjects/root',
+    REST_SUBJECTS_CHILDREN: '/rest/signUp/provinces/{provinceId}/{parentId}/subjects/children'
+}
