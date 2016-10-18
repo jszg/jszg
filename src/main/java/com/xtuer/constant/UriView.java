94850eb9f9c696568be33612cc00d2250ec2c677
microacup <xiangmain@gmail.com>
2016-10-18 14:00:40 +0800

    city->cityId

--- src/main/java/com/xtuer/constant/UriView.java
+++ src/main/java/com/xtuer/constant/UriView.java
@@ -7,7 +7,7 @@ public interface UriView {
     String REST_CERT_TYPE                  = "/rest/signUp/certTypes";
     String REST_PROVINCES                  = "/rest/signUp/provinces";
     String REST_CITIES_BY_PARENT           = "/rest/signUp/provinces/{provinceId}/cities";
-    String REST_ORGS_BY_CITY_AND_CERT_TYPE = "/rest/signUp/orgs/{city}/{certTypeId}";
+    String REST_ORGS_BY_CITY_AND_CERT_TYPE = "/rest/signUp/orgs/{cityId}/{certTypeId}";
     String REST_SUBJECTS_ROOT              = "/rest/signUp/subjects/root/{provinceId}/{certTypeId}";
     String REST_SUBJECTS_CHILDREN          = "/rest/signUp/subjects/children/{provinceId}/{parentId}";
 }
