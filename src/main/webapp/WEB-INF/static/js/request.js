90abf64ca6bdd74c30ae1d2ebae319d68e9d7e70
liangy <liangy@edu-edu.com.cn>
2016-10-19 16:23:39 +0800

    7th

--- src/main/webapp/WEB-INF/static/js/request.js
+++ src/main/webapp/WEB-INF/static/js/request.js
@@ -183,4 +183,8 @@ $(document).ready(function() {
 
     $('#box-1-next').click();
     $('#box-2-next').click();
+    $('#box-3-next').click();
+    $('#box-4-next').click();
+    $('#box-5-next').click();
+     $('#box-6-next').click();
 });
