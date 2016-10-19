ae07be3b15043b1b2b34ffdcc04059a878854711
xtuer <biao.mac@icloud.com>
2016-10-19 21:25:58 +0800

    refactor

--- src/main/java/com/xtuer/controller/UploaderController.java
+++ src/main/java/com/xtuer/controller/UploaderController.java
@@ -5,21 +5,19 @@ import com.xtuer.constant.UriView;
 import org.apache.commons.configuration.PropertiesConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.IOException;
 
 @Controller
 public class UploaderController {
-    @RequestMapping(UriView.URI_UPLOAD_PERSON_IMAGE)
+    @PostMapping(UriView.URI_UPLOAD_PERSON_IMAGE)
     @ResponseBody
-    public Result uploadFile(@RequestParam(name="file", required=false) MultipartFile file, HttpServletRequest request) throws IOException {
+    public Result uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
         System.out.println(file.getOriginalFilename());
         String uploadDir = propertiesConfig.getString("uploadPersonImageDir");
         file.transferTo(new File(uploadDir + File.separator + file.getOriginalFilename()));
