3e8ea6dc1d55de56c2ebae46d997af40cf2d7c62
xtuer <biao.mac@icloud.com>
2016-10-25 09:49:48 +0800

    添加 async 可以手动的使用同步方式请求

--- src/main/webapp/WEB-INF/static/js/jquery.rest.js
+++ src/main/webapp/WEB-INF/static/js/jquery.rest.js
@@ -18,13 +18,14 @@
          * 使用 Ajax 的方式执行 REST 的 GET 请求(服务器响应的数据根据 REST 的规范，应该是 Json 对象).
          * 以下几个 REST 的函数 $.restCreate(), $.restUpdate(), $.restDelete() 只是请求的 HTTP 方法和 data 处理不一样，其他的都是相同的.
          *
-         * @param {[json]} options 有以下几个选项:
-         *        {[string]}   url       请求的 URL(必选)
-         *        {[json]}     urlParams URL 中的变量(可选)，例如 /rest/users/{id}，其中 {id} 为要被 urlParams.id 替换的部分
-         *        {[json]}     data      请求的参数(可选)
-         *        {[function]} success   请求成功时的回调函数(可选)
-         *        {[function]} error     请求失败时的回调函数(可选)
-         *        {[function]} complete  请求完成后的回调函数(可选)
+         * @param {json} options 有以下几个选项:
+         *        {string}   url       请求的 URL(必选)
+         *        {json}     urlParams URL 中的变量(可选)，例如 /rest/users/{id}，其中 {id} 为要被 urlParams.id 替换的部分
+         *        {json}     data      请求的参数(可选)
+         *        {boolean}  async      默认为异步方式(可选)
+         *        {function} success   请求成功时的回调函数(可选)
+         *        {function} error     请求失败时的回调函数(可选)
+         *        {function} complete  请求完成后的回调函数(可选)
          * @return 没有返回值
          */
         get: function(options) {
@@ -53,18 +54,20 @@
          *     2. contentType: 'application/json'
          *     3. data: GET 时为 json 对象，POST, PUT, DELETE 时必须为 JSON.stringify(data)
          *
-         * @param {[json]} options 有以下几个选项:
-         *        {[string]}   url        请求的 URL(必选)
-         *        {[string]}   httpMethod 请求的方式，有 GET, PUT, POST, DELETE(必选)
-         *        {[json]}     urlParams  URL 中的变量(可选)
-         *        {[json]}     data       请求的参数(可选)
-         *        {[function]} success    请求成功时的回调函数(可选)
-         *        {[function]} error      请求失败时的回调函数(可选)
-         *        {[function]} complete   请求完成后的回调函数(可选)
+         * @param {json} options 有以下几个选项:
+         *        {string}   url        请求的 URL(必选)
+         *        {string}   httpMethod 请求的方式，有 GET, PUT, POST, DELETE(必选)
+         *        {json}     urlParams  URL 中的变量(可选)
+         *        {json}     data       请求的参数(可选)
+         *        {boolean}  async      默认为异步方式(可选)
+         *        {function} success    请求成功时的回调函数(可选)
+         *        {function} error      请求失败时的回调函数(可选)
+         *        {function} complete   请求完成后的回调函数(可选)
          */
         sendRequest: function(options) {
             var defaults = {
                 data: {},
+                async: true,
                 success: function() {},
                 error: function() {},
                 complete: function() {}
@@ -84,6 +87,7 @@
             $.ajax({
                 url:  settings.url,
                 data: settings.data,
+                async: settings.async,
                 type: settings.httpMethod,
                 dataType: 'json',
                 contentType: 'application/json'
